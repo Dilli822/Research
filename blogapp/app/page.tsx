30e910f1a84269cf0cd52ef5445c318b16bcea79
dilli <dillihangrae@gmail.com>
2023-12-01 18:16:26 +0545

    [added] adds deploy,,,

---
+++ blogapp/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import "../customCSS/custom.css"; 
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import Footer from "../components/footer";
+import Header from "../components/header";
+import LandingPage from '@/pages/home';
+
+export default function Home() {
+  return (
+<>
+
+
+<LandingPage/>
+
+
+
+
+
+
+
+    
+</>
+
+  )
+}
---
+++ blogapp/components/header.tsx
@@ -0,0 +1,41 @@
+// import "./customCSS/custom.css"; 
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import "./custom.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Header() {
+    return (
+<header className='head sticky-top'>
+  <div className="container-fluid mobContainer"> 
+<div className=" nav d-flex align-items-center">
+  <input type="checkbox" id="nav-check"/>
+  <div className="nav-header">
+    <div className="nav-title">
+      <figure>
+        <img src="https://unsplash.it/500" alt="logo" style={{ width: "50px", height: "50px "}}/>
+      </figure>
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className="nav-links">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </div>
+  </div>
+  </div>
+
+
+</header>
+
+    )}
