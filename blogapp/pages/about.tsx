fe3d003590dda0f271addec42782e1f193e4c062
dilli <dillihangrae@gmail.com>
2023-12-01 22:19:02 +0545

    d

--- blogapp/pages/about.tsx
+++ blogapp/pages/about.tsx
@@ -88,7 +88,7 @@ export default function About() {
 <div className='card-body shadow-lg p-4'>
   <h5 className='card-title'>Card title</h5>
   <h1>87,765</h1>
-  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card' content.</p>
+  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card content.</p>
  
 </div>
 </div>
--- blogapp/pages/contact.tsx
+++ blogapp/pages/contact.tsx
@@ -80,7 +80,7 @@ export default function contact() {
                     <div className='form-group'>
     <label htmlFor='exampleInputEmail1'>Email address</label>
     <input type='email' className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Enter email' required/>
-    <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
+    <small id='emailHelp' className='form-text text-muted'>We never share your email with anyone else.</small>
   </div>
 
   <div className='form-group'>
